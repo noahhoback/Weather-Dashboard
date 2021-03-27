ffe2b00ee3887c53b2b7949ee1e1899c8a713289
Noah Hoback <fullstack47@outlook.com>
2021-03-27 03:34:16 -0400

    ajax calls for temp humidity wind. ajax for UV

--- assets/script.js
+++ assets/script.js
@@ -57,4 +57,35 @@ searchHistoryList.on("click", "search-city-button", function(event) {
     var value = $(this).data("value");
     currentWeatherConditions(userSearchValue);
     searchHistory(value);
+});
+
+function currentWeatherConditions(userSearchValue) {
+    var APIcallURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userSearchValue + "&units=imperial&appid=" + APIkey;
+}
+$.ajax({
+    url: APIcallURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+    currentCity.text(response.name);
+    currentCity.append("<small class='text-muted' id='current-date'>");
+        $("#current-date").text("(" + todaysDate + ")");
+        currentCity.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />" )
+        currentTemperature.text(response.main.temp);
+        currentTemperature.append("&deg;F");
+        currentHumidity.text(response.main.humidity + "%");
+        currentWindSpeed.text(response.wind.speed + "MPH");
+        
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
+
+        var uvURL = "https://api.openweathermap.org/data/2.5/uvi?&lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
+
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function(response){
+            currentUV.text(response.value);
+        }); 
+        })
 })
