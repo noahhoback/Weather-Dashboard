ef9b932160d51bbac27d510d273e04a42f89275a
Noah Hoback <fullstack47@outlook.com>
2021-03-27 02:23:47 -0400

    proper file formatting/declaring variables

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+/* i will need to declare variables for the Weather Dashboard.
+These will include:
+-the  search bar and search button (and a clear button?)
+- all the forecast variables such as temperature, humidity, UV index, etc.
+
+I will then need to figure out a way to grab the users input (city name) in the search bar, and make an API call to the openweather
+API that will then populate the current weather conditions of that city.
+ initial google search suggests using an AJAX call...will need to search more
+
+ however i fetch the forecast info, ill need to append it to the 5 day forcecast display
+
+i will also want to display the current date and time so perhaps using moment.js again unless there is an easier way
+*/
+
+
+
+//listing variables i will need before i start code 
+var searchHistoryList = ;
+var citySearchInput = ;
+var citySearchButton = ; 
+var currentCity = ;
+var currentTemperature = ;
+var currentHumidity = ;
+var currentWindSpeed =;
+var currentUV = ;
+var weatherInfoDiv = ;
+var APIkey = "e1b762965cd10c81e93e8ed2fa42068a" //<- key i got from signing up for openweather
+var todaysDate = ;
